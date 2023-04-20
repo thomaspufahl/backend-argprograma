227e3e318e0a53d11904c041c66a60197f285573
thomaspufahl <thomas.r.pufahl@gmail.com>
2023-04-20 14:41:19 -0300

    fix user builder to set a Role via request

--- src/main/java/com/thomaspufahl/apiportfolio/Security/user/auth/AuthenticationService.java
+++ src/main/java/com/thomaspufahl/apiportfolio/Security/user/auth/AuthenticationService.java
@@ -25,7 +25,7 @@ public class AuthenticationService {
 //                .lastname(request.getLastname())
                 .email(request.getEmail())
                 .password(passwordEncoder.encode(request.getPassword()))
-                .role(Role.USER)
+                .role(request.getRole())
                 .build();
         repository.save(user);
         var jwtToken = jwtService.generateToken(user);
