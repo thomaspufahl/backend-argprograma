97c88e8f63f0cc13b36db523a890e587bd70631f
thomaspufahl <thomas.r.pufahl@gmail.com>
2023-04-20 16:18:13 -0300

    added preAuthorize to Demo for USER authority

--- src/main/java/com/thomaspufahl/apiportfolio/Security/demo/DemoController.java
+++ src/main/java/com/thomaspufahl/apiportfolio/Security/demo/DemoController.java
@@ -1,6 +1,7 @@
 package com.thomaspufahl.apiportfolio.Security.demo;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/demo")
 public class DemoController {
     @GetMapping
+    @PreAuthorize("hasAuthority('USER')")
     public ResponseEntity<String> sayHello() {
         return ResponseEntity.ok("Hello from secured endpoint");
     }
