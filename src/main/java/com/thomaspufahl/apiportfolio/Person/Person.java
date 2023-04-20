32930e2b276be932e1d15b57828ab804b7a33951
thomaspufahl <thomas.r.pufahl@gmail.com>
2023-04-20 12:46:44 -0300

    added person entity

---
+++ src/main/java/com/thomaspufahl/apiportfolio/Person/Person.java
@@ -0,0 +1,21 @@
+package com.thomaspufahl.apiportfolio.Person;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String firstname;
+    private String lastname;
+}
