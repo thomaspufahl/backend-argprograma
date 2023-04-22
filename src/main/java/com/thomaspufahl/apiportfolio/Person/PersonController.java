38f8cfd2cfa4d214ba7228cf070d6d437eb5c9f9
thomaspufahl <thomas.r.pufahl@gmail.com>
2023-04-22 10:56:12 -0300

    update endpoints to return an ResponseEntity

--- src/main/java/com/thomaspufahl/apiportfolio/Person/PersonController.java
+++ src/main/java/com/thomaspufahl/apiportfolio/Person/PersonController.java
@@ -7,41 +7,45 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/person")
 @RequiredArgsConstructor
-@PreAuthorize("hasAuthority('ADMIN')")
+//@PreAuthorize("hasAuthority('ADMIN')")
 public class PersonController {
     private final PersonManager manager;
     @GetMapping
-    public List<Person> getPersons() {return manager.getPersons();}
+    public ResponseEntity<List<Person>> getAll() {
+        return new ResponseEntity<>(manager.getAll(), HttpStatus.OK);
+    }
     @GetMapping("/{person_id}")
-    public Person getPersonById(@PathVariable Integer person_id) {
-        return manager.findPersonById(person_id);
+    public ResponseEntity<Optional<Person>> getById(@PathVariable Integer person_id) {
+        return new ResponseEntity<>(manager.getById(person_id), HttpStatus.OK);
     }
     @PostMapping("/add")
-    public ResponseEntity<Person> addPerson(@RequestParam("firstname") String firstname, @RequestParam("lastname") String lastname) {
+    public ResponseEntity<Person> add(@RequestParam("firstname") String firstname, @RequestParam("lastname") String lastname) {
         Person person = new Person(firstname, lastname);
-        manager.savePerson(person);
+        manager.save(person);
         return new ResponseEntity<>(person, HttpStatus.CREATED);
     }
     @DeleteMapping("/remove")
-    public ResponseEntity<?> removePerson() {
-        manager.deleteAllPersons();
+    public ResponseEntity<String> removeAll() {
+        manager.deleteAll();
         return new ResponseEntity<>("Persons deleted", HttpStatus.OK);
     }
     @DeleteMapping("/remove/{person_id}")
-    public ResponseEntity<?> removePerson(@PathVariable Integer person_id) {
-        manager.deletePersonById(person_id);
+    public ResponseEntity<String> removeById(@PathVariable Integer person_id) {
+        manager.deleteById(person_id);
         return new ResponseEntity<>("Person deleted", HttpStatus.OK);
     }
     @PutMapping("/edit/{person_id}")
-    public Person editPerson(
+    public ResponseEntity<Optional<Person>> editById(
             @PathVariable Integer person_id,
             @RequestParam("firstname") String firstname,
             @RequestParam("lastname") String lastname
     ) {
-        return manager.editPersonById(person_id, firstname, lastname);
+        return new ResponseEntity<>(manager.editById(person_id, firstname, lastname), HttpStatus.OK);
     }
 }
